e21b2df0ae11c064cdcc31683b678dd62c823e47
Krubamax <bghuo2345656@email.com>
2025-04-11 22:44:04 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import rembg
+import numpy as np
+from PIL import Image
+
+input_image = Image.open('S__25550854.jpg')
+input_image = np.array(input_image)
+
+output_array = rembg.remove(input_image)
+output_image = Image.fromarray(output_array)
+
+output_image.save('image/output.png')
+output_image.show()
